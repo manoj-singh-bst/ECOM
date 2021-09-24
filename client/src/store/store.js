969dc0463c3495446520b4242d227848b62e24ec
manoj-singh-bst <manoj0010singh@gmail.com>
2021-09-24 09:44:21 +0530

    qw

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -12,7 +12,7 @@
 
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "../../reducer";
+import rootReducer from "../reducer";
 import thunk from "redux-thunk";
 
 const store = createStore(
