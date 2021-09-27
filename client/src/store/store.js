e85ca9b1cff5dd36f9b688bbd4195f644fa4a765
manoj-singh-bst <manoj0010singh@gmail.com>
2021-09-27 10:48:42 +0530

    add to cart

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,6 +2,7 @@ import {
   getAllProductsReducer,
   getProductByIdReducer,
 } from "../reducer/productReducer";
+import { addToCartReducer } from "../reducer/cartReducer";
 import { combineReducers } from "redux";
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
@@ -10,11 +11,20 @@ import thunk from "redux-thunk";
 const finalReducer = combineReducers({
   getAllProductsReducer: getAllProductsReducer,
   getProductByIdReducer: getProductByIdReducer,
+  addToCartReducer: addToCartReducer,
 });
+const cartItems = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const initialState = {
+  addToCartReducereducer: { cartItems: cartItems },
+};
 const composeEnhancers = composeWithDevTools({});
 
 const store = createStore(
   finalReducer,
+  initialState,
   composeEnhancers(applyMiddleware(thunk))
 );
 export default store;
