c9d3e3ddeefeff6b931bdf1e1858d7f223f2e3b7
manoj-singh-bst <manoj0010singh@gmail.com>
2021-09-29 12:02:38 +0530

    changes

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -5,7 +5,6 @@ import {
 import { addToCartReducer } from "../reducer/cartReducer";
 import { combineReducers } from "redux";
 import logger from "redux-logger";
-
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
