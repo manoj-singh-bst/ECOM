040d361e0b963e560391562507b581ff5beb9372
abhi82855 <abhi82855@gmail.com>
2021-10-01 15:59:51 +0530

    njhkgjgf

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import Rating from "react-rating";
 
 export default function Review() {
+  const [rating, setrating] = useState(5);
+  const [comment, setcomment] = useState("");
+   
+  function sendreview(){
+
+  }
   return (
     <div>
-      <h1>Give your Review</h1>
+      <h2>Give your Review</h2>
+      <Rating
+        style={{ color: "orange" }}
+        initialRating={rating}
+        emptySymbol="fa fa-star-o fa-1x"
+        fullSymbol="fa fa-star fa-1x"
+        readonly={false}
+        onChange={(e) => {
+          setrating(e);
+        }}
+      />
+      <input
+        type="text"
+        className="form-control mt-2 ml-5"
+        value={comment}
+        onChange={(e) => {
+          setcomment(e.target.value);
+        }}
+      ></input>
+      <button className="btn btn-dark mt-5">Submit Review</button>
     </div>
   );
 }
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,4 +1,5 @@
 import {
+  addProductReviewReducer,
   getAllProductsReducer,
   getProductByIdReducer,
 } from "../reducer/productReducer";
@@ -34,6 +35,7 @@ const finalReducer = combineReducers({
   getOrdersByUserIdReducer: getOrdersByUserIdReducer,
   getOrderByIdReducer: getOrderByIdReducer,
   placeOrderReducer: placeOrderReducer,
+  addProductReviewReducer: addProductReviewReducer,
 });
 const currentUser = localStorage.getItem("currentUser")
   ? JSON.parse(localStorage.getItem("currentUser"))
